aa5eadfebf7ef8d3ba26c1f8a3128dacdb73b77b
Waynar Bocangel Calderon <waynarbocangel@Waynars-MBP.lan>
2022-04-05 13:27:48 -0700

    Rename security.js to securityFunctions.js

--- securityFunctions.js
+++ securityFunctions.js
@@ -1,46 +1,46 @@
 /**
- * Security Functions
- * @namespace security
+ * securityFunctions Functions
+ * @namespace securityFunctions
  */
 require("dotenv").config();
-const security = require("crypto-js");
+const securityFunctions = require("crypto-js");
 const schema = require(__dirname + "/schema.js");
 
 /**
  * Hashes the password passed in and then returns it.
- * @memberof security
+ * @memberof securityFunctions
  * @param {String} password The password to hash.
  * @return Returns the hashed password.
  */
 function hashPass (password) {
-    return security.HmacSHA256(password, process.env.HASHKEY).toString();
+    return securityFunctions.HmacSHA256(password, process.env.HASHKEY).toString();
 }
 
 /**
  * Encrypts the message using the password as key and then returns the encrypted message.
- * @memberof security
+ * @memberof securityFunctions
  * @param {String} message The text to encrypt.
  * @param {String} password The password to use as a key.
  * @return The encrypted message.
  */
 function encrypt (message, password) {
-    return security.AES.encrypt(message, password).toString();
+    return securityFunctions.AES.encrypt(message, password).toString();
 }
 
 /**
  * Decrypts the data using the password as key and then returns it.
- * @memberof security
+ * @memberof securityFunctions
  * @param {String} data The text to decrypt.
  * @param {String} password The password to use as key.
  * @return Returns the decrypted data.
  */
 function decrypt (data, password){
-    return security.AES.decrypt(data, password).toString(security.enc.Utf8);
+    return securityFunctions.AES.decrypt(data, password).toString(securityFunctions.enc.Utf8);
 }
 
 /**
  * Authenticates the user based on email and password in userDate
- * @memberof security
+ * @memberof securityFunctions
  * @param {Object} userData The object that contains the email and password to authenticate.
  * @callback (response,error) Sends either true or false based on whether the email and password were authenticated or not, as well as an appropriate error message.
  */
@@ -55,7 +55,7 @@ function authenticate (userData, callback) {
 }
 
 /**
- * @module security
+ * @module securityFunctions
  */
 module.exports = {
     hashPass: hashPass,
