b2b887f1c290e14959f4f474d12de4f03ca92d8e
Waynar Bocangel Calderon <waynarbocangel@Waynars-MBP.lan>
2022-04-06 16:51:21 -0700

    Build functionality added and lintfix quick checker function added to the npm commands

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 require("dotenv").config();
 const express = require("express");
 const schema = require(__dirname + "/schema.js");
-const security = require(__dirname + "/security.js");
+const security = require(__dirname + "/securityFunctions.js");
 const app = express();
 
-app.listen(process.env.PORT, () => {
+function closeServer (serverToClose) {
+	serverToClose.close(() => {
+		process.exit(0);
+	});
+	setTimeout(() => {
+		console.log("Process couldn't close in time");
+		process.exit(1);
+	}, 100000);
+}
+
+let server = app.listen(process.env.PORT, () => {
 	console.log("Started listening at port " + process.env.PORT);
 	console.log(schema);
 	console.log(security);
+	if (process.argv[2] === "build") {
+		setTimeout(() => {
+			closeServer(server);
+		}, 1000);
+	}
 });
 
 app.get("/", (req, res) => {
@@ -17,3 +32,5 @@ app.get("/", (req, res) => {
 app.get("/users/:userID", (req, res) => {
 	res.send("Testing!");
 });
+
+
